8bd987ca33300f8741d80ffee0807c3539a8c098
Muchsin <muchsin.ht@gmail.com>
2016-11-22 21:49:27 +0700

    PopularActivity Part 1

--- app/src/main/java/id/sch/smktelkom_mlg/project/xirpl604132231/project1/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/project/xirpl604132231/project1/MainActivity.java
@@ -1,7 +1,9 @@
 package id.sch.smktelkom_mlg.project.xirpl604132231.project1;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 
 public class MainActivity extends Activity {
@@ -11,5 +13,12 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.imageButtonPopular).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, PopularActivity.class));
+            }
+        });
     }
 }
